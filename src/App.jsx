f67ad9d88a8261ef72e410fe463562f31740ae51
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2024-01-14 20:07:09 +0530

    recoil rerenders

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,56 @@
 import { useContext, useState } from "react"
-import { CountContext } from "./context";
+// import { CountContext } from "./context";
 import { Navigate } from "react-router-dom";
+import { RecoilRoot, useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
+import { countAtom } from "./store/atoms/count";
 
 
 function App() {
-  const [count, setCount] = useState(0);  
+  // const [count, setCount] = useState(0);  
   // wrap anyone that wants to use the teleported value inside a provider
   // recoil, redux, Themes in mUI
   return (
     <div>
-      <CountContext.Provider value={count}>
-        <Count setCount={setCount} />
-      </CountContext.Provider>
+      {/* <CountContext.Provider value={count}> */}
+      <RecoilRoot>
+        <Count /* setCount={setCount} */ />
+      </RecoilRoot>
+      {/* </CountContext.Provider> */}
     </div>
   )
 }
 
-function Count({setCount}) {
+function Count(/* {setCount} */) {
+  console.log("count rerender");
   return <div>
     <CountRenderer />
-    <Buttons setCount={setCount} />
+    <Buttons /* setCount={setCount} */ />
   </div>
 }
 
 function CountRenderer() {
-  const count = useContext(CountContext);
+  // const count = useContext(CountContext);
+  const count = useRecoilValue(countAtom);
   return <div>
     {count}
   </div>
 }
 
-function Buttons({setCount}) {
-  const count = useContext(CountContext);
+function Buttons(/* {setCount} */) {
+  // const count = useContext(CountContext);
+  // const count = 0;
+  // const [count, setCount] = useRecoilState(countAtom);
+  const setCount = useSetRecoilState(countAtom);
+
+  console.log("rerender button");
+
   return <div>
     <button onClick={() => {
-      setCount(count + 1)
+      setCount(count => count + 1)
     }}>Increase</button>
 
     <button onClick={() => {
-      setCount(count - 1)
+      setCount(count => count - 1)
     }}>Decrease</button>
   </div>
 }
